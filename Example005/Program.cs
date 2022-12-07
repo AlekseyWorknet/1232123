1f77b843bfa46555ca8a098cd80f2ff9f131b53f
Alexey Bushuev <dushuev.leha@mail.ru>
2022-12-07 08:48:29 +0300

    условие

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введи имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наташа")
+{
+    Console.WriteLine("Этож Наташа");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
